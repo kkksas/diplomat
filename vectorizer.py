094f785c8219097e991790b401e37e9b9d3788da
Kostya Ivanov <kostya1234kkk@yandex.ru>
2024-10-30 16:23:54 +0300

    сделал норм верторизацию(сделал регулярку)

--- vectorizer.py
+++ vectorizer.py
@@ -9,7 +9,7 @@ def save_in_scv_format(My_Labels , Name):
     np.savetxt(Name, K, delimiter = ',')
 
 df = pd.read_excel('./spreadcheek.xlsx')
-#векторизация тематик
+"""#векторизация тематик
 vectorizerTheme = CountVectorizer()
 theme = vectorizerTheme.fit_transform(df['тематика'])
 #print(vectorizerTheme.get_feature_names_out())
@@ -18,14 +18,15 @@ dfTheme= pd.DataFrame(theme.toarray(), columns=vectorizerTheme.get_feature_names
 print(dfTheme)
 #сохраняем в csv
 #np.savetxt("themeVect.csv", theme.toarray(),fmt='%d',  delimiter=",")
-
+"""
 #векторизация активностей
-vectorizerActivity = CountVectorizer()
+vectorizerActivity = CountVectorizer(token_pattern='(?u)(\\b\\w[\\w\\s]*[^\\,]\\b)*')
 activity = vectorizerActivity.fit_transform(df['активности'].values.astype('U'))
 dfActivity= pd.DataFrame(activity.toarray(), columns=vectorizerActivity.get_feature_names_out())
 print(dfActivity)
-#np.savetxt("activityVect.csv", activity.toarray(),fmt='%d', delimiter=",")
-
+np.savetxt("activityVect.csv", activity.toarray(),fmt='%d', delimiter=",")
+dfActivity.to_csv("activityVect.csv", header=True, sep='\t', encoding='utf-8', index=False)
+"""
 #векторизация городов подумать как их соединять
 vectorizerCity = CountVectorizer()
 cityVect = vectorizerCity.fit_transform(df['Город'].values.astype('U'))
@@ -39,3 +40,4 @@ print(dfLiving)
 conDF = pd.concat([dfTheme, dfActivity, dfCity, dfLiving], axis=1, join='outer')
 print(conDF)
 conDF.to_csv('out.csv', header=True, sep='\t', encoding='utf-8', index=False)
+"""
