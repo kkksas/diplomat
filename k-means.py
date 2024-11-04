c78bf3191c5a79c84c2a0ecd8b58db69b6446aa4
Konstantin <kostya1234kkk@yandex.ru>
2024-11-04 22:32:56 +0300

    рабочая версия силуэта правда только до 13 кластеров почему-то

--- k-means.py
+++ k-means.py
@@ -22,43 +22,70 @@ plt.xlabel(u'Величина K')
 plt.ylabel(u'Силуэтная отметка')
 plt.show()
 """
-def silhouette(N_clusters):
-    cls = KMeans(n_clusters=N_clusters, random_state=1).fit(df1)
+def silhouette(cls):
+    #cls = KMeans(n_clusters=N_clusters, random_state=1).fit(df1)
     return silhouette_score(df1, cls.labels_, metric='euclidean')
 
-
-def closeness(N_clusters):
-    cls = KMeans(n_clusters=N_clusters, random_state=1).fit(df1)
+def closeness2(N_clusters, cls):
+    #cls = KMeans(n_clusters=N_clusters, random_state=1).fit(df1)
     mean_inter_claster=[]
-    for i in range(N_clusters):
-        mask = cls.labels_==i
-        labeledRec = df1[mask]
-        dist = euclidean_distances(labeledRec, labeledRec)
-        sum_dist = np.sum(dist, axis=1)
-        mean_dist = sum_dist / (dist.size-1)
-        mean_inter_claster.append(np.mean(mean_dist))
-    a = np.mean(mean_inter_claster, axis=0)
-    print(a)
-    return a 
-
-def farness(N_clusters):
-    cls = KMeans(n_clusters=N_clusters, random_state=1).fit(df1)
-    nearest_cluster_distance = []
-    for i in range(N_clusters):        
-        mask = cls.labels_==i
+    iter = 0 
+    for i in df1.to_numpy():
+        mask = cls.labels_==cls.labels_[iter]
         labeledRec = df1[mask].to_numpy()
-        for j in labeledRec:
-            foreign_clusters = np.delete(cls.cluster_centers_, i,0)
-            second_closest_cluster = foreign_clusters[np.argmin(euclidean_distances(j.reshape(1, -1), foreign_clusters),axis=1)]
-            index=-1
-            for k in range(N_clusters):
-                if (cls.cluster_centers_[k]== second_closest_cluster).all():
-                    index = k 
-                    break
-            scc_mask = cls.labels_==index
-            scc_align_rec = df1[scc_mask].to_numpy()
-            nearest_cluster_distance.append(np.mean(euclidean_distances(j.reshape(1, -1), scc_align_rec))) 
-    return np.mean(nearest_cluster_distance)           
+        dist = euclidean_distances(i.reshape(1, -1), labeledRec)
+        sum_dist = np.sum(dist, axis=1)
+        if dist.size != 1:
+            mean_dist = sum_dist / (dist.size-1)
+        else:
+            mean_dist = 0
+        mean_inter_claster = np.append(mean_inter_claster, mean_dist)
+        iter += 1
+    return mean_inter_claster
+        
 
-print(silhouette(2))
-print(My_silhouette(2))
+def farness2(N_clusters, cls):
+    #cls = KMeans(n_clusters=N_clusters, random_state=1).fit(df1)
+    nearest_cluster_distance = []
+    labeledRec = []
+    for i in range(N_clusters):
+        mask = cls.labels_ == i 
+        labeledRec.append(df1[mask].to_numpy())
+    for i in df1.to_numpy():
+        cluster_dists = []
+        leng = len(labeledRec)
+        for j in range(leng):
+            clust_rec = labeledRec[j]
+            eu = euclidean_distances(i.reshape(1, -1), clust_rec)
+            if (0 in eu):
+                cluster_dists= np.append(cluster_dists, 0)
+                continue
+            cluster_dists =np.append(cluster_dists, np.mean(eu))
+        nearest_cluster_distance = np.append(nearest_cluster_distance, min(cluster_dists[cluster_dists != 0]))
+    return nearest_cluster_distance
+
+def mySel(N_clusters, cls):
+    a1=closeness2(N_clusters,cls)
+    b1=farness2(N_clusters,cls)
+    return np.mean((b1-a1)/np.maximum(a1,b1)) 
+
+Ks = range(2, 14)
+Ds = []
+Ds2 = []
+ases= []
+inertia = []
+for K in Ks:
+ cls = KMeans(n_clusters=K, random_state=1).fit(df1)
+ labels = cls.labels_
+ Ds2.append(mySel(K, cls))
+ Ds.append(silhouette_score(df1, labels, metric='euclidean'))
+
+plt.plot(Ks, Ds, 'o-')
+plt.plot(Ks, Ds2, 'x-')
+plt.xlabel(u'Величина K')
+plt.ylabel(u'Силуэтная отметка')
+plt.show()
+
+cls = KMeans(n_clusters=20, random_state=1).fit(df1)
+labels = cls.labels_
+mySel(20, cls)
