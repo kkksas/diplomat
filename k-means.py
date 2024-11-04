f99c09a9ba78b0d6fa98b5bfa6bad7b4c8b92c4b
Kostya Ivanov <kostya1234kkk@yandex.ru>
2024-11-04 12:18:39 +0300

    потраченного времени жаль

--- k-means.py
+++ k-means.py
@@ -4,15 +4,61 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import homogeneity_completeness_v_measure
 from sklearn.metrics import silhouette_score
+from sklearn.metrics.pairwise import euclidean_distances
 df1 = pd.read_excel('./prav.xlsx')
-
+#оно дает слишком большой вклад 
+#df1['время трансфера'] = df1['время трансфера'] / 24
+"""
 Ks = range(2, 15)
 Ds = []
+inertia = []
 for K in Ks:
  cls = KMeans(n_clusters=K, random_state=1).fit(df1)
  labels = cls.labels_
+ inertia.append(cls.inertia_)
  Ds.append(silhouette_score(df1, labels, metric='euclidean'))
 plt.plot(Ks, Ds, 'o-')
 plt.xlabel(u'Величина K')
 plt.ylabel(u'Силуэтная отметка')
 plt.show()
+"""
+def silhouette(N_clusters):
+    cls = KMeans(n_clusters=N_clusters, random_state=1).fit(df1)
+    return silhouette_score(df1, cls.labels_, metric='euclidean')
+
+
+def closeness(N_clusters):
+    cls = KMeans(n_clusters=N_clusters, random_state=1).fit(df1)
+    mean_inter_claster=[]
+    for i in range(N_clusters):
+        mask = cls.labels_==i
+        labeledRec = df1[mask]
+        dist = euclidean_distances(labeledRec, labeledRec)
+        sum_dist = np.sum(dist, axis=1)
+        mean_dist = sum_dist / (dist.size-1)
+        mean_inter_claster.append(np.mean(mean_dist))
+    a = np.mean(mean_inter_claster, axis=0)
+    print(a)
+    return a 
+
+def farness(N_clusters):
+    cls = KMeans(n_clusters=N_clusters, random_state=1).fit(df1)
+    nearest_cluster_distance = []
+    for i in range(N_clusters):        
+        mask = cls.labels_==i
+        labeledRec = df1[mask].to_numpy()
+        for j in labeledRec:
+            foreign_clusters = np.delete(cls.cluster_centers_, i,0)
+            second_closest_cluster = foreign_clusters[np.argmin(euclidean_distances(j.reshape(1, -1), foreign_clusters),axis=1)]
+            index=-1
+            for k in range(N_clusters):
+                if (cls.cluster_centers_[k]== second_closest_cluster).all():
+                    index = k 
+                    break
+            scc_mask = cls.labels_==index
+            scc_align_rec = df1[scc_mask].to_numpy()
+            nearest_cluster_distance.append(np.mean(euclidean_distances(j.reshape(1, -1), scc_align_rec))) 
+    return np.mean(nearest_cluster_distance)           
+
+print(silhouette(2))
+print(My_silhouette(2))
--- vectorizer.py
+++ vectorizer.py
@@ -9,8 +9,8 @@ def save_in_scv_format(My_Labels , Name):
     np.savetxt(Name, K, delimiter = ',')
 
 df = pd.read_excel('./spreadcheek.xlsx')
-"""#векторизация тематик
-vectorizerTheme = CountVectorizer()
+#векторизация тематик
+vectorizerTheme = CountVectorizer(token_pattern='(?u)(\\b\\w[\\w\\s]*[^\\,\\-]\\b)*')
 theme = vectorizerTheme.fit_transform(df['тематика'])
 #print(vectorizerTheme.get_feature_names_out())
 #print(theme.toarray())
@@ -18,26 +18,24 @@ dfTheme= pd.DataFrame(theme.toarray(), columns=vectorizerTheme.get_feature_names
 print(dfTheme)
 #сохраняем в csv
 #np.savetxt("themeVect.csv", theme.toarray(),fmt='%d',  delimiter=",")
-"""
+
 #векторизация активностей
-vectorizerActivity = CountVectorizer(token_pattern='(?u)(\\b\\w[\\w\\s]*[^\\,]\\b)*')
+vectorizerActivity = CountVectorizer(token_pattern='(?u)(\\b\\w[\\w\\s]*[^\\,\\-]\\b)*')
 activity = vectorizerActivity.fit_transform(df['активности'].values.astype('U'))
 dfActivity= pd.DataFrame(activity.toarray(), columns=vectorizerActivity.get_feature_names_out())
 print(dfActivity)
-np.savetxt("activityVect.csv", activity.toarray(),fmt='%d', delimiter=",")
-dfActivity.to_csv("activityVect.csv", header=True, sep='\t', encoding='utf-8', index=False)
-"""
+#dfActivity.to_csv("activityVect.csv", header=True, sep='\t', encoding='utf-8', index=False)
+
 #векторизация городов подумать как их соединять
-vectorizerCity = CountVectorizer()
+vectorizerCity = CountVectorizer(token_pattern='(?u)(\\b\\w[\\w\\s]*[^\\,\\-]\\b)*')
 cityVect = vectorizerCity.fit_transform(df['Город'].values.astype('U'))
 dfCity= pd.DataFrame(cityVect.toarray(), columns=vectorizerCity.get_feature_names_out())
 print(dfCity)
 #векторизация проживания если от названий избавится то будет норм
-vectorizerLiving = CountVectorizer()
+vectorizerLiving = CountVectorizer(token_pattern='(?u)(\\b\\w[\\w\\s]*[^\\,\\-]\\b)*')
 livingVect = vectorizerLiving.fit_transform(df['проживание'].values.astype('U'))
 dfLiving= pd.DataFrame(livingVect.toarray(), columns=vectorizerLiving.get_feature_names_out())
 print(dfLiving)
 conDF = pd.concat([dfTheme, dfActivity, dfCity, dfLiving], axis=1, join='outer')
 print(conDF)
 conDF.to_csv('out.csv', header=True, sep='\t', encoding='utf-8', index=False)
-"""
