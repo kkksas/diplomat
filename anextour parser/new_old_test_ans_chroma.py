401be21faa633a2ac9027c7a86178339f0661ea8
Kostya Ivanov <kostya12345kkk@yandex.ru>
2025-12-08 23:08:45 +0300

    for 9_12

--- anextour parser/new_old_test_ans_chroma.py
+++ anextour parser/new_old_test_ans_chroma.py
@@ -11,8 +11,8 @@ import numpy as np
 import matplotlib
 K_c = 10
 
-is_metric_type_mine = False
-query_addon = "Дан вопрос, необходимо найти абзац текста с ответом \nвопрос:"
+is_metric_type_mine = True
+query_addon = 'Given a web search query, retrieve relevant passages that answer the query'
 def acc_t(x):
     a = list(pd.Series(x['ret_chunks']).isin(x['relv']).dropna()) 
     return sum(a)/len(a)   
@@ -31,12 +31,12 @@ def acc_buttom(qds) -> float:
     return a.astype(int)
 
 def create_emb(emb):
-    cds = pd.read_excel('chunked.xlsx', usecols=['id','context']).dropna()
+    cds = pd.read_excel('chunked_all.xlsx', usecols=['id','context']).dropna()
     cds['len_c'] = cds['context'].apply(lambda x: len(x))
     documents = []
     for i in cds.iloc:
         documents.append(Document(page_content=i['context'], metadata = {"id":int(i['id'])}))
-    chroma_db = Chroma.from_documents(documents, emb, persist_directory='./chromadb_new', collection_name='v_russ')
+    chroma_db = Chroma.from_documents(documents, emb, persist_directory='./chromadb_all', collection_name='v_russ')
     chroma_db.persist()
     return chroma_db
 
@@ -66,9 +66,10 @@ s_time = time.time()
 #######___faiss___#########################
 # model = AutoModel.from_pretrained("ai-sage/Giga-Embeddings-instruct",trust_remote_code=True)
 model_kwargs = {'device': 'cuda'} 
-emb = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-large-instruct", model_kwargs=model_kwargs)#intfloat/multilingual-e5-large-instruct, ai-sage/Giga-Embeddings-instruct
+emb = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-large-instruct",  model_kwargs=model_kwargs)#intfloat/multilingual-e5-large-instruct, ai-sage/Giga-Embeddings-instruct
 #загрузить эмбы
-chroma_db = Chroma(persist_directory='./chromadb_full', collection_name='the_world', embedding_function=emb)
+# chroma_db = Chroma( persist_directory='./chromadb_all', collection_name='v_russ', embedding_function=emb)
+chroma_db = create_emb(emb)
 def tokenize(s: str) -> list[str]:
     """Очень простая функция разбития предложения на слова"""
     return s.lower().translate(str.maketrans("", "", string.punctuation)).split(" ")
@@ -100,14 +101,14 @@ for ds_num in range(1,5):
     qds = pd.read_csv(f'./new_ds{ds_num}.csv', dtype=str, sep=';' ).dropna()
     qds['relv'] = qds['relv'].apply(lambda x: list(map(int, x.split(','))))
     for k_c in range(1, K_c+1):
-        retriever = chroma_db.as_retriever(search_kwargs={'k':k_c})
+        retriever = chroma_db.as_retriever(search_type="similarity_score_threshold", search_kwargs={'k':k_c, 'score_threshold': 0.784})
         bm25_retriever.k=k_c
         ensemble_retriever = EnsembleRetriever(
         retrievers=[retriever, bm25_retriever],
         weights=[0.8, 0.2],
         tags=['e5', 'bm25']
         )         
-        qds['ret_chunks'] = qds["question"].apply(lambda x: list(elem.metadata['id'] for elem in ensemble_retriever.invoke(x, k=k_c+1)[:k_c])) 
+        qds['ret_chunks'] = qds["question"].apply(lambda x: list(elem.metadata['id'] for elem in ensemble_retriever.invoke(query_addon+x, k=k_c+1)[:k_c])) 
         if is_metric_type_mine:
             qds[f'acc_{k_c}'] = acc_top(qds)
         else:
@@ -115,7 +116,7 @@ for ds_num in range(1,5):
     res_df = pd.concat([res_df, qds], axis=0, ignore_index=True)
             
 # result = pd.DataFrame(full_res, columns=[i for i in range(1, K_c+1)])
-res_df.to_excel(f'results/new/kc_ans_alt.xlsx', index=False)
+res_df.to_excel(f'results/new/kc_ans.xlsx', index=False)
 
 end_time =time.time()   
 print("total_spended:", end_time-s_time)
